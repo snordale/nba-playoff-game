70cbcf0fba19af2ec99ba9f599ebb5342c4c2ac5
Snordale <snordale@gmail.com>
2025-04-15 11:46:22 +0300

    Trigger build

--- app/api/cron/load-games/route.ts
+++ app/api/cron/load-games/route.ts
@@ -2,7 +2,9 @@ import { loadGamesForDate } from "@/services/GameLoaderService";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  console.log("Loading games for today");
   const targetDate = new Date();
   await loadGamesForDate(targetDate);
+  console.log("Games loaded for today");
   return NextResponse.json("Get some");
 }
